2c0a9e56bfc58272b2c395b5d335b9c01a79b761
Delath <delath.707@gmail.com>
2024-03-31 21:09:26 +0200

    Remove tmux in favor of official rcon support

--- papermc/papermc.sh
+++ papermc/papermc.sh
@@ -17,16 +17,10 @@ if [[ -z "$ALLOCATED_RAM" ]]; then
     ALLOCATED_RAM="4G"
 fi
 
-# Create a new tmux session
-tmux new-session -d -s "papermc"
-
 # Download the latest Paper jar and launch it with Aikar's Flags -> https://docs.papermc.io/paper/aikars-flags
-tmux send-keys -t "papermc" "cd /home/papermc/world" Enter
-tmux send-keys -t "papermc" "curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/${LATEST_VERSION}/builds/${LATEST_BUILD}/downloads/${JAR_NAME}" Enter
-tmux send-keys -t "papermc" "echo 'eula=${EULA}' > eula.txt" Enter
-tmux send-keys -t "papermc" "chown -R papermc:papermc /home/papermc" Enter
-tmux send-keys -t "papermc" "chmod +x paper.jar" Enter
-tmux send-keys -t "papermc" "su -c 'java -Xmx${ALLOCATED_RAM} -Xms${ALLOCATED_RAM} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui' papermc" Enter
-
-# Attach to the tmux session
-tmux attach -t "papermc"
+cd /home/papermc/world
+curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/${LATEST_VERSION}/builds/${LATEST_BUILD}/downloads/${JAR_NAME}
+echo "eula=${EULA}" > eula.txt
+chown -R papermc:papermc /home/papermc
+chmod +x paper.jar
+su -c "java -Xmx${ALLOCATED_RAM} -Xms${ALLOCATED_RAM} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper.jar --nogui" papermc
